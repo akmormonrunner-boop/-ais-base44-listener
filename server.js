0dadf345cf4c94e6c56508ccfc6b307c07219f45
akmormonrunner-boop <akmormonrunner@icloud.com>
2026-07-19 17:09:54 -0600

    Update server.js

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ function connectToAisStream() {
   socket.on("close", (code, reasonBuffer) => {
   const reason = reasonBuffer
    ?.toString() 
-   "No reason provided";
+   :"No reason provided";
 
   console.log(
     `AISstream disconnected. Code: ${code}. Reason: ${reason}`
